f45a92999802612778d0e982a2bc2c115c7a9772
adultfishchen <adultfish17640336@gmail.com>
2021-04-28 15:22:31 +0800

    try add

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ if __name__ == "__main__":
     DAN.deregister()
     exit()
 
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
