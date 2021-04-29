d6d1f88cb5db8571e611f76507c5ab9bd2806732
adultfishchen <adultfish17640336@gmail.com>
2021-04-29 20:20:56 +0800

    modify handle

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ if __name__ == "__main__":
     t1.start()
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,threaded=True)
